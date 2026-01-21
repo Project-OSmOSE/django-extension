5d26809445ff824352b6c8875fee3499b1509c30
Elodie Morin <elodie.morin1298@gmail.com>
2026-01-21 16:23:55 +0100

    Update views

---
+++ src/django_extension/views/init.py
@@ -0,0 +1,13 @@
+from rest_framework.renderers import BaseRenderer
+
+__all__ = [
+    'CSVRenderer',
+]
+
+class CSVRenderer(BaseRenderer):
+    """Custom renderer for CSV files"""
+
+    media_type = "text/csv"
+
+    def render(self, data, accepted_media_type=None, renderer_context=None):
+        return "\n".join([",".join(line) for line in data])

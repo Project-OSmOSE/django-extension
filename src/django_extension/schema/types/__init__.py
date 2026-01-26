a832e5571c784ac819b7c3f9eaa3b14f321ed0e9
Elodie Morin <elodie.morin1298@gmail.com>
2026-01-26 13:59:07 +0100

    clean(schema): OldExtendedNode

--- src/django_extension/schema/types/__init__.py
+++ src/django_extension/schema/types/__init__.py
@@ -1,5 +1,3 @@
-from django_extension.schema.errors import NotFoundError
-
 from .enum import ExtendedEnumType
 
 __all__ = [
@@ -9,67 +7,11 @@ __all__ = [
     'ExtendedNode',
 ]
 
-from typing import Optional
-
 from django.db.models import QuerySet
 from django_extension.schema.interfaces import ExtendedInterface
-from graphene_django.types import DjangoObjectTypeOptions
 import graphene_django_optimizer
 from graphql import GraphQLResolveInfo
 from graphene_django import DjangoObjectType
-from rest_framework.request import Request
-
-
-class ModelContextFilter:
-    """Base context filter"""
-
-    @classmethod
-    def get_queryset(cls, context: Request):
-        return QuerySet().none()
-
-    @classmethod
-    def get_edit_node_or_fail(cls, context: Request, pk: int):
-        """Get node with edit rights or fail depending on the context"""
-        raise NotFoundError()
-
-
-class ExtendedObjectTypeMeta(DjangoObjectTypeOptions):
-    context_filter: Optional[ModelContextFilter.__class__] = None
-
-
-class OldExtendedNode(DjangoObjectType):
-    """Base object type"""
-
-    class Meta:
-        abstract = True
-
-
-    @classmethod
-    def __init_subclass_with_meta__(
-            cls,
-            context_filter: Optional[ModelContextFilter.__class__] = None,
-            _meta=None,
-            **kwargs,
-    ):
-        if not _meta:
-            _meta = ExtendedObjectTypeMeta(cls)
-        if context_filter is not None:
-            _meta.context_filter = context_filter
-        super().__init_subclass_with_meta__(_meta=_meta, **kwargs)
-
-    @classmethod
-    def resolve_queryset(cls, queryset: QuerySet, info: GraphQLResolveInfo):
-        """Resolve Queryset"""
-        if cls._meta.context_filter is None:
-            return queryset
-        return cls._meta.context_filter.get_queryset(info.context, queryset=queryset)
-
-    @classmethod
-    def get_queryset(cls, queryset: QuerySet, info: GraphQLResolveInfo):
-        """Get Queryset"""
-        return graphene_django_optimizer.query(
-            cls.resolve_queryset(queryset, info), info
-        )
 
 
 class ExtendedNode(DjangoObjectType):
@@ -85,7 +27,8 @@ class ExtendedNode(DjangoObjectType):
                                     convert_choices_to_enum=None, _meta=None, **options):
         super().__init_subclass_with_meta__(model, registry, skip_registry, only_fields, fields, exclude_fields,
                                             exclude, filter_fields, filterset_class, connection, connection_class,
-                                            use_connection, interfaces + (ExtendedInterface,), convert_choices_to_enum, _meta, **options)
+                                            use_connection, interfaces + (ExtendedInterface,), convert_choices_to_enum,
+                                            _meta, **options)
 
     @classmethod
     def resolve_queryset(cls, queryset: QuerySet, info: GraphQLResolveInfo):
@@ -102,5 +45,3 @@ class ExtendedNode(DjangoObjectType):
         return graphene_django_optimizer.query(
             cls.resolve_queryset(queryset, info), info
         )
-
-

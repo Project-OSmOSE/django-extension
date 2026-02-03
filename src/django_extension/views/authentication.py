458c133bd61f379d7a6f47c0263aff9eef586308
Elodie Morin <elodie.morin@ensta-bretagne.fr>
2026-02-03 15:40:31 +0100

    v0.1.2 - fix DRFAuthenticatedGraphQLView authentication

--- src/django_extension/views/authentication.py
+++ src/django_extension/views/authentication.py
@@ -1,8 +1,16 @@
-from rest_framework.authentication import TokenAuthentication
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication
 
 __all__ = [
-    'BearerTokenAuthentication'
+    'BearerTokenAuthentication',
+    'CsrfExemptSessionAuthentication'
 ]
 
 class BearerTokenAuthentication(TokenAuthentication):
     keyword = "Bearer"
+
+class CsrfExemptSessionAuthentication(SessionAuthentication):
+    """SessionAuthentication subclass that skips CSRF validation."""
+
+    def enforce_csrf(self, request):
+        # Skip CSRF check entirely
+        return

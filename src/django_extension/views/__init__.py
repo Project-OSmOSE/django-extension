82ccaf0467e0af8f1fd33e65bbc40b75a165725c
Elodie Morin <elodie.morin@ensta-bretagne.fr>
2026-02-03 15:40:31 +0100

    v0.1.1 - fix DRFAuthenticatedGraphQLView authentication

--- src/django_extension/views/__init__.py
+++ src/django_extension/views/__init__.py
@@ -5,8 +5,9 @@ from rest_framework import permissions
 from rest_framework.decorators import permission_classes, authentication_classes, api_view
 from rest_framework.request import Request
 from rest_framework.settings import api_settings
+from rest_framework.authentication import SessionAuthentication
 
-from .authentication import BearerTokenAuthentication
+from .authentication import BearerTokenAuthentication, CsrfExemptSessionAuthentication
 from .renderer import CSVRenderer
 
 logger = logging.getLogger(__name__)
@@ -22,6 +23,16 @@ __all__ = [
 ]
 
 
+def _get_graphql_authentication_classes():
+    """
+    Returns the default authentication classes with SessionAuthentication
+    replaced by CsrfExemptSessionAuthentication.
+    """
+    return [
+        CsrfExemptSessionAuthentication if cls is SessionAuthentication else cls
+        for cls in api_settings.DEFAULT_AUTHENTICATION_CLASSES
+    ]
+
 class DRFAuthenticatedGraphQLView(GraphQLView):
     """Allow GraphQL to handle REST authenticated users"""
 
@@ -34,6 +45,6 @@ class DRFAuthenticatedGraphQLView(GraphQLView):
     def as_view(cls, *args, **kwargs):
         view = super().as_view(*args, **kwargs)
         view = permission_classes((permissions.AllowAny,))(view)
-        view = authentication_classes(api_settings.DEFAULT_AUTHENTICATION_CLASSES)(view)
+        view = authentication_classes(_get_graphql_authentication_classes())(view)
         view = api_view(["GET", "POST"])(view)
         return view
